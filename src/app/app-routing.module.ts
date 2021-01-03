d6bb08a3f999e90abc92b22a9943f03600b14a91
tituanamarys <maritzaeli20@gmail.com>
2021-01-03 12:59:09 -0500

    Creacion de navegacion entre paginas y opciones de la primera pantalla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+    
+  {
+    path: 'asistente',
+    loadChildren: () => import('./asistente/asistente.module').then( m => m.AsistentePageModule)
   }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
---
+++ src/app/asistente/asistente.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asistente',
+  templateUrl: './asistente.page.html',
+  styleUrls: ['./asistente.page.scss'],
+})
+export class AsistentePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
